ccf80f5ff062e51acb9841df7c4ee73917c6f86f
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-01-07 21:42:28 +0200

    drivers/android: binder: freecess: use new freezer v2 logic
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -20,6 +20,7 @@
 #include <asm/cacheflush.h>
 #include <linux/list.h>
 #include <linux/sched/mm.h>
+#include <linux/sched/jobctl.h>
 #include <linux/module.h>
 #include <linux/rtmutex.h>
 #include <linux/rbtree.h>
@@ -438,13 +439,13 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 
 #ifdef CONFIG_SAMSUNG_FREECESS
 	if (is_async && (alloc->free_async_space < 3*(size + sizeof(struct binder_buffer))
-		|| (alloc->free_async_space < ((alloc->buffer_size/2)*9/10)))) {
+		|| (alloc->free_async_space < alloc->buffer_size/4))) {
 		struct task_struct *p;
 
 		rcu_read_lock();
 		p = find_task_by_vpid(alloc->pid);
 		rcu_read_unlock();
-		if (p && (thread_group_is_frozen(p)))
+		if (p && (thread_group_is_frozen(p) || p->jobctl & JOBCTL_TRAP_FREEZE))
 			binder_report(p, -1, "free_buffer_full", is_async);
 	}
 #endif
